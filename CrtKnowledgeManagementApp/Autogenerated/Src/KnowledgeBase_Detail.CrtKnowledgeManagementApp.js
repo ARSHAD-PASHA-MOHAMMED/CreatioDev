7bf54b427a776bf4c37a63f00b9b4a1b79cac47d
ARSHAD-PASHA-MOHAMMED <arshad.pasha@inovar.sa>
2025-12-09 16:26:15 +0300

    save

--- CrtKnowledgeManagementApp/Autogenerated/Src/KnowledgeBase_Detail.CrtKnowledgeManagementApp.js
+++
@@ -1,8 +0,0 @@
-﻿define('KnowledgeBase_Detail', [], function () {
-	return {
-		entitySchemaName: 'KnowledgeBase',
-		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
-		methods: {},
-	};
-});
